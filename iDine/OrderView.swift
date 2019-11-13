3d3b55598517bbc722d3d64d1f57f3d41c6c8f93
Mantas <mantas.skeiverys@gmail.com>
2019-11-13 15:27:50 +0200

    Initial Commit

---
+++ iDine/OrderView.swift
@@ -0,0 +1,36 @@
+//
+//  OrderView.swift
+//  iDine
+//
+//  Created by T1001 on 2019-11-13.
+//  Copyright © 2019 T1001. All rights reserved.
+//
+
+import SwiftUI
+
+struct OrderView : View {
+   @EnvironmentObject var order: Order
+   var body: some View {
+      NavigationView {
+         List {
+            Section {
+                ForEach(order.items) { item in HStack {
+                    Text(item.name)
+                    Spacer()
+                    Text("$\(item.price)")
+                    }
+                }
+            }
+            Section {
+                NavigationLink(destination: Text("Check out")) {
+                    Text("Place Order") }
+            }
+         }
+         .navigationBarTitle("Order")
+        .listStyle(GroupedListStyle()) }
+    }
+}
+
+struct OrderView_Previews: PreviewProvider { static let order = Order()
+static var previews: some View { OrderView().environmentObject(order)
+} }
